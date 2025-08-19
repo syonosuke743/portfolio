a173acee7df30f09edde6d9cd54fb935739f3c59
syonosuke743 <syonosukeeee@gmail.com>
2025-08-19 08:00:12 +0000

    manifest.jsonの追加

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,9 +1,13 @@
-//macOSではボリュームマウントが遅く、ホットリロードが効きづらい場合がある
+// next.config.js
+const withPWA = require("next-pwa")({
+    dest: "public", // Service Worker を出力する場所
+    register: true, // 自動登録
+    skipWaiting: true, // 新しいSWがすぐ反映される
+    disable: process.env.NODE_ENV === "development", // dev環境では無効
+  });
 
-const nextConfig = {
-};
-
-export default nextConfig;
-
+  module.exports = withPWA({
+    reactStrictMode: true,
+  });
 
 
