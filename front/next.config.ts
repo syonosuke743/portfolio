feb10400849c8413a1bd814ab0437a841c660fd7
syonosuke743 <syonosukeeee@gmail.com>
2025-08-20 01:47:57 +0000

    PWA対応完了

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,9 +1,9 @@
-// next.config.js
+
 const withPWA = require("next-pwa")({
     dest: "public", // Service Worker を出力する場所
     register: true, // 自動登録
     skipWaiting: true, // 新しいSWがすぐ反映される
-    disable: process.env.NODE_ENV === "development", // dev環境では無効
+    disable: false
   });
 
   module.exports = withPWA({
