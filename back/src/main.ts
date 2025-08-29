16bf443106edbdc2961df59f9d6829f28ba1f950
syonosuke743 <syonosukeeee@gmail.com>
2025-08-29 01:42:58 +0000

    test

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 
 
-//test
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
