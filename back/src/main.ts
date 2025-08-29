4592f33fe68a6d68dc5a76bad8d0e131a424b67b
syonosuke743 <syonosukeeee@gmail.com>
2025-08-29 01:20:48 +0000

    test

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 
-//cicdのtestコメント
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
